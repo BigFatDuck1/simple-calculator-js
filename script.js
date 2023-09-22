56cca2f60e27e23c832904c74ebba2c658b5e3c3
BigFatDuck1 <clementho999@gmail.com>
2023-09-22 14:57:04 +0800

    Added functions for individual operations

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//Create functions for add, subtract, multiply, divide
+let add = (a,b) => {
+    return a + b;
+}
+
+let subtract = (a,b) => {
+    return a - b;
+}
+
+let multiply = (a,b) => {
+    return a * b;
+}
+
+let divide = (a,b) => {
+    if (b == 0) {
+        return "error";
+    }
+    return a / b;
+}
+
+console.log(divide(5,2));
